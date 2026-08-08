a7a0b6b784653c2c7dea0eb197e36105b7a47761
justinmmorgan21 <justinmorgan21@gmail.com>
2026-08-08 12:34:52 -0500

    Add Firestore initialization and export
    Export Firestore database instance for use in the app.

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAh-wIZ8bag2rB2_kJ7gzsBURT48rpQrAc",
   authDomain: "ap-cs-a-adaptive-system.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:583658215718:web:152bbfa09783c864b249e7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
