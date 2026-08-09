d8d54b6aa0cc5f4d8696c47a452c9279a4e3523b
justinmmorgan21 <justinmorgan21@gmail.com>
2026-08-08 21:10:02 -0500

    Refactor item imports to include segment items

--- src/items/index.js
+++ src/items/index.js
@@ -1,9 +1,11 @@
 // Combines every unit's item file into one bank. When a new unit's
 // questions are ready, add an import + spread it in below -- nothing
 // else in the app needs to change.
-import { UNIT1_ITEMS } from "./unit1";
+import { UNIT1_SEGMENT_A_ITEMS } from "./unit1-segmentA";
+import { UNIT1_SEGMENT_B_ITEMS } from "./unit1-segmentB";
 
 export const ITEM_BANK = [
-  ...UNIT1_ITEMS,
+  ...UNIT1_SEGMENT_A_ITEMS,
+  ...UNIT1_SEGMENT_B_ITEMS,
   // ...UNIT2_ITEMS,  <- add future units here
 ];
