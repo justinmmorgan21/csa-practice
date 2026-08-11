713cbdcc421e84544600b343c10dc1a8fc84ec65
justinmmorgan21 <justinmorgan21@gmail.com>
2026-08-11 08:07:08 -0500

    Add UNIT1_SEGMENT_C_ITEMS to ITEM_BANK

--- src/items/index.js
+++ src/items/index.js
@@ -3,9 +3,11 @@
 // else in the app needs to change.
 import { UNIT1_SEGMENT_A_ITEMS } from "./unit1-segmentA";
 import { UNIT1_SEGMENT_B_ITEMS } from "./unit1-segmentB";
+import { UNIT1_SEGMENT_C_ITEMS } from "./unit1-segmentC";
 
 export const ITEM_BANK = [
   ...UNIT1_SEGMENT_A_ITEMS,
   ...UNIT1_SEGMENT_B_ITEMS,
+  ...UNIT1_SEGMENT_C_ITEMS,
   // ...UNIT2_ITEMS,  <- add future units here
 ];
