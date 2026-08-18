e1f985e9bfc5b8f7b79e9fb3e97ca207959603cd
justinmmorgan21 <justinmorgan21@gmail.com>
2026-08-17 19:55:23 -0500

    Add UNIT2_SEGMENT_B_ITEMS to index.js

--- src/items/index.js
+++ src/items/index.js
@@ -5,6 +5,7 @@ import { UNIT1_SEGMENT_A_ITEMS } from "./unit1-segmentA";
 import { UNIT1_SEGMENT_B_ITEMS } from "./unit1-segmentB";
 import { UNIT1_SEGMENT_C_ITEMS } from "./unit1-segmentC";
 import { UNIT2_SEGMENT_A_ITEMS } from "./unit2-segmentA";
+import { UNIT2_SEGMENT_B_ITEMS } from "./unit2-segmentB";
 
 // This is the seed/fallback content -- the live, editable version now lives
 // in Firestore (see contentStore.js). This static bank is only used to
@@ -15,5 +16,6 @@ export const STATIC_ITEM_BANK = [
   ...UNIT1_SEGMENT_B_ITEMS,
   ...UNIT1_SEGMENT_C_ITEMS,
   ...UNIT2_SEGMENT_A_ITEMS,
-  // ...UNIT2_SEGMENT_B_ITEMS,  <- add future segments here
+  ...UNIT2_SEGMENT_B_ITEMS,
+  // <- add future segments here
 ];
